b7f783e3f324aab9e9b269a4b2b6210deb6eb94c
MarvinHuber <Marvin.Huber@edu.gbssg.ch>
2024-05-17 11:08:50 +0200

    improved design (social buttons are still missing)

---
+++ navbar.js
@@ -0,0 +1,34 @@
+// JavaScript to handle button clicks
+document.getElementById("homeBtn").addEventListener("click", function() {
+    window.location.href = "home.html"; // Change to your home page URL
+});
+
+document.getElementById("aboutBtn").addEventListener("click", function() {
+    window.location.href = "ueber_mich.html"; // Change to your Über mich page URL
+});
+
+document.getElementById("skillsBtn").addEventListener("click", function() {
+    window.location.href = "skills.html"; // Change to your skills page URL
+});
+
+document.getElementById("portfolioBtn").addEventListener("click", function() {
+    window.location.href = "portfolio.html"; // Change to your portfolio page URL
+});
+
+document.getElementById("contactBtn").addEventListener("click", function() {
+    window.location.href = "contact.html"; // Change to your contact page URL
+});
+
+
+/* Style for the scroll to top button */
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollToTopBtn = document.querySelector("#scrollToTopBtn");
+
+    // When the button is clicked, scroll to the top of the page
+    scrollToTopBtn.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // Smooth scrolling behavior
+        });
+    });
+});
